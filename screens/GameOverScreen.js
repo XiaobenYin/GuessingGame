d707d45f9dcf21a37f387e9f9384d574abef6bf2
Xiaoben Yin <70068293+XiaobenYin@users.noreply.github.com>
2022-10-02 18:40:01 -0700

    finished game ending screen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -5,31 +5,72 @@ import {
   Button,
   Modal,
   SafeAreaView,
+  Image,
 } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import { color } from "../style/Helper";
 import Card from "../components/Card";
-import Header from "../components/Header"
+import Header from "../components/Header";
 
-
-
-export default function GameOverScreen({ modal, onNewGame, onGuessNumber, onTargetNumber }) {
-  const notice = "Game is over";
+function winOrNot(guess, target) {
+  if (guess != target) {
     return (
+      <Image
+        style={styles.tinyLogo}
+        source={require("../assets/Sad-emoji.png")}
+      />
+    );
+  } else {
+    return (
+      <Image
+        style={styles.tinyLogo}
+        source={{
+          uri: "https://picsum.photos/id/" + target + "/100/100",
+        }}
+      />
+    );
+  }
+}
+
+export default function GameOverScreen({
+  modal,
+  onNewGame,
+  onGuessNumber,
+  onTargetNumber,
+}) {
+  const notice = "Game is over";
+  const img = winOrNot(onGuessNumber, onTargetNumber);
+  return (
     <View style={styles.container}>
-        <Header appName={notice}/>
+      <Header appName={notice} />
       <Card>
-        <Button title="Start Again" onPress={onNewGame}></Button>
+        <Text style={styles.instruction}>Here's your picture</Text>
+        {img}
+        <Button
+          title="Start Again"
+          color="#FF1493"
+          onPress={onNewGame}
+        ></Button>
       </Card>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      padding: 16,
-      marginTop: 30,
-    },
-  });
-  
+  container: {
+    flex: 1,
+    padding: 16,
+    marginTop: 30,
+  },
+  tinyLogo: {
+    marginTop: 20,
+    marginBottom: 10,
+    width: 80,
+    height: 80,
+  },
+  instruction: {
+    marginTop: 10,
+    color: color.gold,
+    fontSize: 20,
+  },
+});
